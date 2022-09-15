387a29ea296efcf7bfa48392fd8bbe7aca122360
flexxxender <flexxxender@gmail.com>
2022-09-15 23:09:13 +0300

    create laboratory work models

--- Models/LaboratoryWork.cs
+++ Models/LaboratoryWork.cs
@@ -6,8 +6,15 @@ namespace maihelper.Models
     {
         [Key]
         public int Id { get; set; }
+
+        public int Views { get; set; } = 0;
+        public int OptionLab { get; set; } = 0;  //Вариант лабораторной работы
         public string? Name { get; set; }
         public string? FilePath { get; set; }
+        public string? DownloadFile { get; set; }
+        public string? Teacher { get; set; }
+        public string? Author { get; set; }
+        public string? Listing { get; set; }    //Листинг (ссылка на path)
 
     }
 }
