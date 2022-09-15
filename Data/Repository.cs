bd78b98921eebea203cbdad3bacd6841e0fd3bf5
SlepoyShaman <ilushin905@gmail.com>
2022-09-16 00:35:26 +0300

    create a repository class

---
+++ Data/Repository.cs
@@ -0,0 +1,15 @@
+﻿namespace maihelper.Data
+{
+    public class Repository
+    {
+        private readonly ApplicationDbContext _context;
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IQueryable<TEntity> GetAll<TEntity>() where TEntity : class
+            => _context.Set<TEntity>().Select(p => p);
+
+    }
+}
